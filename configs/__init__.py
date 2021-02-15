d10cae81e356605668357786563be1bc1d2e287f
Denis <komedian@bk.ru>
2021-02-15 17:07:17 +0300

    iam dataset full pack

--- configs/__init__.py
+++ configs/__init__.py
@@ -1,9 +1,11 @@
 # -*- coding: utf-8 -*-
 from .config_bentham import BenthamConfig
 from .config_peter import PeterConfig
+from .config_iam import IAMConfig
 
 
 CONFIGS = {
     'bentham': BenthamConfig,
     'peter': PeterConfig,
+    'iam': IAMConfig,
 }
---
+++ runners/iam/prepare_stackmix.sh
@@ -0,0 +1,7 @@
+# if you need resume experiment, should set checkpoint_path
+python scripts/prepare_stackmix.py \
+  --dataset_name "iam" \
+  --data_dir "../StackMix-OCR-DATA" \
+  --mwe_tokens_dir "../StackMix-OCR-MWE_TOKENS" \
+  --image_w 2048 \
+  --image_h 128
